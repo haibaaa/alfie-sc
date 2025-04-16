fdb072892aa1e9cdc9a91ab0633fcfbc2a3d00bf
haiba <9700samarth@gmail.com>
2025-04-16 23:32:17 +0530

    hopefully unbroken production

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,9 +4,6 @@ import { db } from "~/server/db";
 export const dynamic = "force-dynamic";
 
 export default async function HomePage() {
-  const posts = await db.query.posts.findMany()
-  console.log(posts);
-
   return (
     <main className="">
       <div>
