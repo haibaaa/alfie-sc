07c497fbb60ab3ae7336f7b4e3fef70a4d6e91d1
haiba <9700samarth@gmail.com>
2025-04-16 22:49:12 +0530

    force dynamic page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,12 @@
 import Link from "next/link";
 import { db } from "~/server/db";
 
+export const dynamic = "force-dynamic";
+
 export default async function HomePage() {
   const posts = await db.query.posts.findMany()
   console.log(posts);
-  
+
   return (
     <main className="">
       <div>
