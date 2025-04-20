8e5178947bae1acbc79494e3c0e425612bc0918c
haiba <9700samarth@gmail.com>
2025-04-20 16:09:05 +0530

    dash file structure

--- src/app/main/layout.tsx
+++ src/app/main/layout.tsx
@@ -24,25 +24,15 @@
 
 "use client";
 
-import { Menu } from "lucide-react";
-import { AppSidebar } from "~/components/app-sidebar";
+import { AppSidebar } from "~/app/main/_components/app-sidebar";
 import { useState } from "react";
 
 export default function Layout({ children }: { children: React.ReactNode }) {
-  const [sidebarOpen, setSidebarOpen] = useState(true);
-  
   return (
     <div className="flex min-h-screen">
-      {sidebarOpen && <AppSidebar />}
-      
+      {<AppSidebar />}
       <main className="flex-1">
         <div className="p-4">
-          <button 
-            onClick={() => setSidebarOpen(!sidebarOpen)}
-            className="inline-flex items-center justify-center w-10 h-10 rounded-md mb-4 bg-background hover:bg-accent"
-          >
-            <Menu className="h-4 w-4" />
-          </button>
           {children}
         </div>
       </main>
